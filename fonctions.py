12a83333c3897185d6ab2ed66047f8da2f0d9671
Robin Muller <robin.muller.romutech@gmail.com>
2019-05-26 02:38:26 +0200

    feature: programming functions of game

--- fonctions.py
+++ fonctions.py
@@ -1,21 +1,83 @@
-def register_player_name(player_name):
-	print("todo")
+import pickle
+import random
 
 def choose_word():
-	print("todo")
+	with open('data', 'rb') as file:
+		my_depickler = pickle.Unpickler(file)
+		words = my_depickler.load()
+	return random.choice(words)
 
-def make_hidden_word(secret_word):
-	print("todo")
+def init_hidden_word(secret_word):
+	i = 0
+	hidden_word = ''
+	while i < len(secret_word):
+		i += 1
+		hidden_word += '*'
+	return hidden_word
+
+def make_hidden_word(secret_word, letter, hidden_word):
+	i = 0
+	hidden_word_bis = ''
+	for letter_word in secret_word:
+		if letter_word == letter :
+			hidden_word_bis = hidden_word_bis + letter
+		elif hidden_word[i] ==  '*': 
+			hidden_word_bis = hidden_word_bis + '*'
+		else:
+			hidden_word_bis = hidden_word_bis + hidden_word[i]
+		i += 1
+
+	hidden_word = hidden_word_bis
+	print('le mot')
+	print(hidden_word_bis)
+	return hidden_word
 
 def letter_is_in_word(secret_word, letter):
-	print("todo")
+	if letter in secret_word:
+		return 1
+	return 0
 
-def word_is_not_found(hidden_word) :
-	print("todo")
+def word_not_found(hidden_word) :
+	if '*' in hidden_word:
+		return 1
+	return 0
 
-def recording_player_score(player_name) :
-	print("todo")
+def recording_player_score(player_name, points) :
+	with open('score', 'rb') as file:
+		my_depickler = pickle.Unpickler(file)
+		try:
+			score = my_depickler.load()
+			if score.get(player_name) == None:
+				score[player_name] = points
+			else:
+				value = score.get(player_name)
+				score[player_name] = value + points
+		except EOFError as e:
+			score = {player_name: points}
 
-def player_score_reading(player_name) :
-	print("todo")
-	
+	with open('score', 'wb') as file:
+		my_depickler = pickle.Pickler(file)
+		my_depickler.dump(score)
+
+def reading_player_score(player_name) :
+	with open('score', 'rb') as file:
+		my_depickler = pickle.Unpickler(file)
+		try:
+			score = my_depickler.load()
+		except EOFError as e:
+			return 0
+		
+		return score.get(player_name)	
+		
+def reading_scores() :
+	print("LES SCORES : ")
+	with open('score', 'rb') as file:
+		my_depickler = pickle.Unpickler(file)
+		try:
+			return my_depickler.load()
+		except Exception as e:
+			return 'Pas de score pour le moment'
+
+
+
+
--- pendu.py
+++ pendu.py
@@ -4,39 +4,50 @@ from fonctions import *
 shots = 8
 word_is_not_found = 1
 loop = 1
+lost = 0
 
 print("Bonjour et bienvenue dans le jeu du pendu")
 player_name = str(input("saisissez votre nom : "))
-register_player_name(player_name)
-print("C'est parti ! Vous avez 8 chances pour deviner le mot mystère.")
 secret_word = choose_word()
-hidden_word = make_hidden_word(secret_word)
+hidden_word = init_hidden_word(secret_word)
+print("C'est parti {} ! Vous avez {} chances pour deviner le mot mystère.".format(player_name, shots))
 
 while(loop) :
-	while shots > 0 and word_is_not_found:
+	while shots > 0 and lost == 0:
+		if shots < 8:
+			print("Il vous reste {} chances pour deviner le mot mystère.".format(shots))
+
 		print(hidden_word)
-		letter = input("Proposez une lettre qui pourrait être contenu dans le mot mystère")
+		letter = str(input("Proposez une lettre qui pourrait être contenu dans le mot mystère: "))
 		
-		if letter_is_in_word(secret_word, letter) :
-			hidden_word = make_hidden_word(secret_word, letter)
-			word_is_not_found = word_is_not_found(hidden_word)
+		if letter_is_in_word(secret_word, letter):
+			print('Bravo la lettre est dans le mot')
+			hidden_word = make_hidden_word(secret_word, letter, hidden_word)
+			word_is_not_found = word_not_found(hidden_word)
 		else :
+			print('Désolé la lettre n\'est pas dans le mot')
 			shots -= 1
 
-	if word_is_not_found :
-		print("Désolé vous avez perdu ! Votre score reste inchangé.")
-	else :
-		recording_player_score(player_name)
-		print("Bravo ! Vous avez gagnez")
-		print("Votre score est maintenant de " + player_score_reading(player_name))
+		if word_is_not_found and shots == 0 :
+			lost = 1
+			print("Désolé vous avez perdu ! Votre score reste inchangé.")
+			print('il est de {} points'.format(reading_player_score(player_name)))
+		elif word_is_not_found == 0 and shots > 0:
+			recording_player_score(player_name, shots)
+			print("Bravo ! Vous avez gagnez")
+			print("Votre score est maintenant de {} points".format(reading_player_score(player_name)))
+			shots = 0
 
 	answer_to_continue = str(input("Voulez-vous rejouer ? (Taper \"oui\" ou \"non\")"))
 
 	if answer_to_continue == "oui" :
 		word_is_not_found = 1
 		shots = 8
+		lost = 0
 		secret_word = choose_word()
-		hidden_word = make_hidden_word(secret_word)
+		hidden_word = init_hidden_word(secret_word)
 	elif answer_to_continue == "non" :
 		print("Au revoir !")
 		loop = 0
+
+	print(reading_scores())
